c4a65edeaea2f297bcdda091c47b3195ae581aa7
Martin Habovstiak <martin.habovstiak@gmail.com>
2019-09-30 10:34:14 +0200

    Added basic debconf support

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
 extern crate configure_me_codegen;
 
 fn main() {
-    configure_me_codegen::build_script_with_man("config_spec.toml").unwrap();
+    if let Some(mut man_dir) = std::env::var_os("MAN_DIR") {
+        std::fs::create_dir_all(&man_dir).expect("Failed to create directory target/man");
+        man_dir.push("btc_rpc_proxy.1");
+        configure_me_codegen::build_script_with_man_written_to("config_spec.toml", man_dir)
+    } else {
+        configure_me_codegen::build_script("config_spec.toml")
+    }.expect("Failed to generate code");
 }
