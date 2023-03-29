245f9dd062445a1ea1d2c440732d35e3373a7d72
Martin Habovstiak <martin.habovstiak@gmail.com>
2023-03-29 15:29:27 +0200

    Fix test

--- src/users.rs
+++ src/users.rs
@@ -324,6 +324,7 @@ mod tests {
             password: "secret".try_into().expect("failed to create password"),
             allowed_calls: HashSet::new(),
             fetch_blocks: input,
+            override_wallet: None,
         });
 
         let result = super::input::map_default(users, default);
